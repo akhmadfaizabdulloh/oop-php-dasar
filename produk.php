bf9bb028693164da1aca71c1db9f1ad53f0ce62c
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-11-05 17:57:48 +0700

    Property & Method Finish

--- produk.php
+++ produk.php
@@ -11,6 +11,16 @@ class Produk {
 
     // method adalah function yang ada di dalam class
 
+    // public function sayhello() {
+    //     return "Hello World!";
+    // }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+
+        // $this : berfungsi untuk mengambil isi dari properti yg ada didalam class yg bersangkutan, ketika dibuat instansi 
+    }
+
 }
 
 // $produk1 = new Produk();
@@ -31,4 +41,22 @@ $produk3->harga = 30000;
 // var_dump($produk3);
 
 
-echo "Komik : $produk3->penulis, $produk3->penerbit";
+// echo "Komik : $produk3->penulis, $produk3->penerbit";
+
+// echo "<br>";
+// echo $produk3->sayhello();
+
+// echo $produk3->getLabel();
+
+
+// echo "<hr>";
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Drucmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
